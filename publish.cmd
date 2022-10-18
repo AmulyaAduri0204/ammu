3c2ee21e35f15371bfa3fe981600ea670c4fd92a
AmulyaAduri0204 <111121880+AmulyaAduri0204@users.noreply.github.com>
2022-10-18 12:50:23 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\amulya-bot-9bad-Web-Deploy.pubxml -p:Password=DbJixHxmt6XJxQshehankjvBkcSbYCaRcAjY6yXScs0vTvNYmfEY3oqiEH07
+
